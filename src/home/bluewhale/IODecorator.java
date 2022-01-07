03b7d8a25c3fe9959ff75827dd37630906d21027
blueWhale <info@teh-dozor.ru>
2022-01-07 13:57:50 +0300

    decorator
    tmp

---
+++ src/home/bluewhale/IODecorator.java
@@ -0,0 +1,32 @@
+package home.bluewhale;
+
+import home.bluewhale.decorator.io.LowerCaseInputStream;
+
+import java.io.*;
+
+public class IODecorator {
+
+    public static void main(String[] args) {
+        int c;
+
+        try {
+
+            File file = new File("resources/test.txt");
+            InputStream in =
+                    new LowerCaseInputStream(
+                            new BufferedInputStream(
+                                    new FileInputStream(file)
+                            )
+                    );
+
+            while ((c = in.read()) >= 0) {
+                System.out.print((char) c);
+            }
+
+            in.close();
+
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+    }
+}
